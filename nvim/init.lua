b4c7560b00a15d77292a83b15abf33c59b40ca36
FARAZ <faraz@FARAZs-MacBook.local>
2026-09-12 14:21:34 +0330

    fix tab suggestion bug

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,21 +6,56 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.termguicolors = true
 
-vim.opt.clipboard = "unnamedplus"
-
 vim.g.mapleader = " "
+
+vim.opt.spell = true
+vim.opt.spelllang = "en"
+
+
+-- Scrolling
+vim.opt.scrolloff = 2
+vim.opt.sidescrolloff = 2
+vim.opt.smoothscroll = true
+
+-- Don't wrap long lines
+vim.opt.wrap = false
+
+-- Mouse
+vim.opt.mouse = "a"
+
 
 
 -----------------------------------------------------------
--- Lazy.nvim Setup
+-- vim-visual-multi
+-----------------------------------------------------------
+
+vim.g.VM_maps = {
+  ["Find Under"]         = "<C-d>",
+  ["Find Subword Under"] = "<C-d>",
+  ["Select All"]         = "<C-S-l>",
+  ["Add Cursor Down"]    = "<leader>j",
+  ["Add Cursor Up"]      = "<leader>k",
+  ["Skip Region"]        = "<C-x>",
+  ["Remove Region"]      = "<C-p>",
+  ["Undo"]               = "u",
+  ["Redo"]               = "<C-r>",
+
+  -- Tab is intentionally NOT used by CMP.
+  -- Visual Multi can still use it for switching mode.
+  ["Switch Mode"]        = "<Tab>",
+}
+
+
+-----------------------------------------------------------
+-- Lazy.nvim
 -----------------------------------------------------------
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -45,116 +80,225 @@ vim.opt.rtp:prepend(lazypath)
 -----------------------------------------------------------
 
 require("lazy").setup({
+  -- Diagnostics
 {
-  "williamboman/mason.nvim",
+  "rachartier/tiny-inline-diagnostic.nvim",
+  event = "LspAttach",
+  opts = {},
 },
-{
-  "windwp/nvim-autopairs",
-},
-{
-  "windwp/nvim-ts-autotag",
-},
-{
-  "williamboman/mason-lspconfig.nvim",
-},
+  -- Multi cursor
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+  },
+  -- Icons
+  {
+    "nvim-tree/nvim-web-devicons",
+  },
 
-{
-  "neovim/nvim-lspconfig",
-},
+  -- Mason
+  {
+    "williamboman/mason.nvim",
+  },
 
-{
-  "hrsh7th/nvim-cmp",
-},
+  -- Auto pairs
+  {
+    "windwp/nvim-autopairs",
+  },
 
-{
-  "hrsh7th/cmp-nvim-lsp",
-},
+  -- Auto HTML/JSX tags
+  {
+    "windwp/nvim-ts-autotag",
+  },
 
-{
-  "L3MON4D3/LuaSnip",
-},
+  -- LSP
+  {
+    "williamboman/mason-lspconfig.nvim",
+  },
 
-{
-  "saadparwaiz1/cmp_luasnip",
-},
+  {
+    "neovim/nvim-lspconfig",
+  },
+
+  -- Completion
+  {
+    "hrsh7th/nvim-cmp",
+  },
+
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+
+  {
+    "L3MON4D3/LuaSnip",
+  },
+
+  {
+    "saadparwaiz1/cmp_luasnip",
+  },
+
   -- File explorer
   {
     "nvim-tree/nvim-tree.lua",
+
     dependencies = {
-      "nvim-tree/nvim-web-devicons"
+      "nvim-tree/nvim-web-devicons",
     },
   },
-  -- for comment 
-{
-  "numToStr/Comment.nvim",
-  opts = {},
-},
+
+  -- Comments
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
+  },
 
   -- Status bar
   {
     "nvim-lualine/lualine.nvim",
   },
 
-
-  -- Better syntax highlighting
+  -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "master",
     build = ":TSUpdate",
   },
 
-
-  -- Search files
+  -- AI completion
   {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim"
-    },
+    "supermaven-inc/supermaven-nvim",
+
+    config = function()
+      require("supermaven-nvim").setup({})
+    end,
   },
 
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 
   -- Theme
   {
     "folke/tokyonight.nvim",
   },
 
-
 })
 
 
 -----------------------------------------------------------
--- Plugin Config
+-- Plugin Configuration
 -----------------------------------------------------------
 
+-- Mason
 require("mason").setup()
+
+-----------------------------------------------------------
+-- Diagnostics / Error Lens
+-----------------------------------------------------------
+
+require("tiny-inline-diagnostic").setup({
+  preset = "modern",
+})
+
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
+
 -- Theme
 vim.cmd.colorscheme("tokyonight")
 
+
+-- Icons
+require("nvim-web-devicons").setup({
+  override = {},
+  default = true,
+})
+
+
 -- File tree
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  renderer = {
+    icons = {
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+    },
+  },
+})
 
 
 -- Status line
 require("lualine").setup()
+
+
+-- Comments
 require("Comment").setup()
+
+
+-- Auto pairs
+require("nvim-autopairs").setup()
+
+
+-- Auto HTML/JSX tags
+require("nvim-ts-autotag").setup()
+
+
+-----------------------------------------------------------
+-- Comment Keymaps
+-----------------------------------------------------------
 
 vim.keymap.set("n", "<leader>/", function()
   require("Comment.api").toggle.linewise.current()
-end, { desc = "Toggle comment" })
+end, {
+  desc = "Toggle comment",
+})
+
+
+vim.keymap.set("v", "<leader>/", function()
+  local esc = vim.api.nvim_replace_termcodes(
+    "<ESC>",
+    true,
+    false,
+    true
+  )
+
+  vim.api.nvim_feedkeys(esc, "nx", false)
+
+  require("Comment.api").toggle.linewise(
+    vim.fn.visualmode()
+  )
+end, {
+  desc = "Toggle comment",
+})
+
+
+-----------------------------------------------------------
+-- Format
+-----------------------------------------------------------
+
 vim.keymap.set("n", "<leader>n", function()
   vim.lsp.buf.format({ async = true })
-end, { desc = "Format file" })
-vim.keymap.set("v", "<leader>/", function()
-  local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
-  vim.api.nvim_feedkeys(esc, "nx", false)
-  require("Comment.api").toggle.linewise(vim.fn.visualmode())
-end, { desc = "Toggle comment" })
-require("nvim-autopairs").setup()
-require("nvim-ts-autotag").setup()
+end, {
+  desc = "Format file",
+})
+
+
 -----------------------------------------------------------
 -- Keymaps
 -----------------------------------------------------------
 
--- Open file tree
+-- File tree
 vim.keymap.set("n", "<leader>e", function()
   local api = require("nvim-tree.api")
 
@@ -163,50 +307,73 @@ vim.keymap.set("n", "<leader>e", function()
   else
     api.tree.open()
   end
-end)
-vim.keymap.set("n", "<leader>l", "<C-w>l")
+end, {
+  desc = "File Explorer",
+})
+
+
+-- Move to right window
+vim.keymap.set("n", "<leader>l", "<C-w>l", {
+  desc = "Move right",
+})
+
+
+-- Delete/change visual selection
 vim.keymap.set("v", "<BS>", "c")
--- Telescope find files
-vim.keymap.set(
-  "n",
-  "<leader>f",
-  ":Telescope find_files<CR>"
-)
 
 
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
-
+-- Telescope
+vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>", {
+  desc = "Find files",
+})
 
 
 -- Terminal
-vim.keymap.set(
-  "n",
-  "<leader>t",
-  ":terminal<CR>"
-)
+vim.keymap.set("n", "<leader>t", ":terminal<CR>", {
+  desc = "Terminal",
+})
 
 
+-- Exit terminal mode
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], {
+  noremap = true,
+  silent = true,
+})
 
 
+-----------------------------------------------------------
+-- LSP
+-----------------------------------------------------------
+
+local capabilities =
+  require("cmp_nvim_lsp").default_capabilities()
+
+
+-- TypeScript / JavaScript
 vim.lsp.config("ts_ls", {
-  capabilities = require("cmp_nvim_lsp").default_capabilities()
+  capabilities = capabilities,
 })
 
 vim.lsp.enable("ts_ls")
 
+
+-- HTML
 vim.lsp.config("html", {
-  capabilities = require("cmp_nvim_lsp").default_capabilities()
+  capabilities = capabilities,
 })
 
 vim.lsp.enable("html")
 
 
+-- CSS
 vim.lsp.config("cssls", {
-  capabilities = require("cmp_nvim_lsp").default_capabilities()
+  capabilities = capabilities,
 })
 
 vim.lsp.enable("cssls")
 
+
+-- Emmet
 vim.lsp.config("emmet_language_server", {
   filetypes = {
     "html",
@@ -221,28 +388,40 @@ vim.lsp.config("emmet_language_server", {
 
 vim.lsp.enable("emmet_language_server")
 
+
+-----------------------------------------------------------
+-- nvim-cmp
+-----------------------------------------------------------
+
 local cmp = require("cmp")
 
 cmp.setup({
+  completion = {
+    completeopt = "menu,menuone,noinsert",
+  },
 
-  mapping = cmp.mapping.preset.insert({
+  mapping = {
+    ["<Down>"] = cmp.mapping.select_next_item({
+      behavior = cmp.SelectBehavior.Select,
+    }),
 
-    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<Up>"] = cmp.mapping.select_prev_item({
+      behavior = cmp.SelectBehavior.Select,
+    }),
 
-    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<CR>"] = cmp.mapping.confirm({
+      select = true,
+    }),
 
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    
-    ["<C-Space>"] = cmp.mapping.complete(), 
-  }),
-
+    ["<C-Space>"] = cmp.mapping.complete(),
+  },
 
   sources = {
-
-    { name = "nvim_lsp" },
-
-    { name = "luasnip" },
-
-  }
-
+    {
+      name = "nvim_lsp",
+    },
+    {
+      name = "luasnip",
+    },
+  },
 })
